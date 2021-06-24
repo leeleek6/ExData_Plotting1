4eb28c172c28c663d19ba410f7a090a814fb3567
leeleek6 <85255966+leeleek6@users.noreply.github.com>
2021-06-24 10:38:01 -0400

    Update plot1.R
    Added comments

--- plot1.R
+++ plot1.R
@@ -1,7 +1,13 @@
+##read data
 hpc <- read.delim("household_power_consumption.txt", header = TRUE, sep = ";")
+##format Date as date and Global_active_power as numeric
 hpc$Date <- dmy(hpc$Date)
 hpc$Global_active_power <- as.numeric(hpc$Global_active_power)
+##subset into required dates
 hpc <- subset(hpc, Date == "2007-02-01" | Date == "2007-02-02")
+##create png
 png("plot1.png")
+##create plot
 hist(hpc$Global_active_power,col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
+##close connection
 dev.off()
